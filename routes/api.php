7f0dbc2b1cc079cc4d818e9024ec6b7fc7c6a689
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2016-11-22 14:57:16 +0400

    remove alta route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Middleware\CheckTicketAuth;
 
-Route::get('/stations', 'StationController@index');
+Route::get('stations', 'StationController@index');
 Route::post('/stations/refresh', 'StationController@refresh');
 Route::post('/stations/clear', 'StationController@clear');
 
