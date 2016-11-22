933fc883fc3a1676d0540c01a481c0c40c337aa2
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2016-11-22 15:47:46 +0400

    remove alta route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -37,6 +26,3 @@ Route::group(['prefix' => 'raconsole', 'middleware' => 'admin'], function()
     Route::get('transaction/ticket/{ticket}/html', 'Admin\TransactionCrudController@html');
     Route::post('transaction/ticket/{ticket}/sync', 'Admin\TransactionCrudController@sync');
 });
-
-
-//require('api.php');
