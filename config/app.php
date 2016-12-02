759a36b02f69751ad91a425909f37911199d49dc
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2016-12-02 18:10:37 +0400

    Dashboard time

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'Matarebeli.ge',
 
     /*
     |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Tbilisi',
+    'timezone' => 'GMT +4',
 
     /*
     |--------------------------------------------------------------------------
