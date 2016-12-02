0daf043ddef52a971408729be1fd332f69941acd
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2016-12-02 18:52:17 +0400

    Dashboard time

--- app/Http/Controllers/Admin/TransactionCrudController.php
+++ app/Http/Controllers/Admin/TransactionCrudController.php
@@ -179,9 +179,9 @@ class TransactionCrudController extends CrudController {
             ],
             function( $value ) {
                 if($value)
-                    $this->crud->addClause('where', 'status', $value);
+                    $this->crud->addClause( 'where', 'status', $value );
                 else
-                    $this->crud->addClause('where', 'status', '>', Transaction::$pending);
+                    $this->crud->addClause( 'where', 'status', '>', Transaction::$pending );
             });
 
         $this->crud->addFilter([
