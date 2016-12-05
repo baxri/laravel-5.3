78cf1bc8713daf5a95fe0384d6f48130ca6842fb
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2016-12-05 12:29:58 +0400

    transaction:clear task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('transaction:clear')->everyMinute();
+        $schedule->command('transaction:clear')
+            ->evenInMaintenanceMode()
+            ->everyMinute();
     }
 
     /**
