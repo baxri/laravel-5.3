1ba6e129124a08990c74ef9022efc69cb8894ac3
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2016-12-05 15:00:54 +0400

    transaction:clear task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('transaction:clear')
-            ->everyMinute()
+            ->daily()
             ->appendOutputTo(
                 config('railway.task_transaction_clear')
                 .'transaction-clear/transaction.clear' );
