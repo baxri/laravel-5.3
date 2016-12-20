cacb9a4cc776116ba68a8665b0403c37be88707f
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2016-12-20 12:45:00 +0400

    Language For Checkout

--- app/Gateway/Payment.php
+++ app/Gateway/Payment.php
@@ -54,10 +54,10 @@ class Payment
 
             $form_data['Hash'] = $this->_hash($form_data);
 
-            $form_data['Items[]'] = $params['items'][0];
+            $form_data['Items[]'] = $params['items'];
 
-            if( isset($params['items'][1]) )
-                $form_data['Items[]'] = $params['items'][1];
+            //if( isset($params['items'][1]) )
+                //$form_data['Items[]'] = $params['items'][1];
 
             $stations = $this->client->request('POST', $this->gateWay.'/createorder', [
                 'form_params' => $form_data
