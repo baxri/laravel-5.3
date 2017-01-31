5cfbec5ed7772acfe465f921a3a7033b99ac5450
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-01-31 14:53:41 +0400

    # in back urls

--- resources/views/vendor/backpack/crud/details/inc/tickets_row.blade.php
+++ resources/views/vendor/backpack/crud/details/inc/tickets_row.blade.php
@@ -93,8 +93,6 @@
 
                     button.button('reset');
 
-                    console.log(result.railway_status);
-
                     if( result.data.railway_status == {{\App\Ticket::$railway_active_status}} ){
                         new PNotify({
                             title: "მიმდინარე სტატუსი",
