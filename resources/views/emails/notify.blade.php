d6f422416ef462b5a4b6a2d2cf5ea16a72699b12
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-02-09 12:19:06 +0400

    Ticket Autorized

--- resources/views/emails/notify.blade.php
+++ resources/views/emails/notify.blade.php
@@ -1,107 +1,61 @@
-<table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#e6e6e6" style="border:none;border-collapse:collapse">
-    <tbody><tr height="50">
-        <td style="border:none;border-collapse:collapse" align="center">
-            <table>
-                <tbody><tr>
-                    <td height="50" width="100%" align="center"></td>
-                </tr>
-                </tbody></table>
-        </td>
-    </tr>
-    <tr height="150">
-        <td align="center">
-            <table bgcolor="#ffffff" style="border:1px solid #cccccc;width:90%;max-width:600px;border:none;border-collapse:collapse">
-                <tbody><tr>
-                    <td height="20" width="100%" align="left" colspan="3" bgcolor="#ffb953"></td>
-                </tr>
-                <tr>
-                    <td width="5%" align="center" bgcolor="#ffb953"></td>
-                    <td align="left" width="90%" bgcolor="#ffb953">
-                        <img src="https://ci4.googleusercontent.com/proxy/Uh801qqI4S9ux00f70wAN6uWEycNS_VNqPJPNUKMLkAd69ZjlnErLTBb7xHaKXrmVG21eIViLeDE-ZgCHV3PNm_qGA31G3hQAw=s0-d-e1-ft#http://www.matarebeli.ge/images/matarebeli_logo.png" alt="UniPAY" width="119" height="24" class="CToWUd">
-                    </td>
-                    <td width="5%" align="center" bgcolor="#ffb953"></td>
-                </tr>
-                <tr>
-                    <td height="5" width="100%" align="left" colspan="3" bgcolor="#ffb953"></td>
-                </tr>
-                <tr>
-                    <td width="5%" align="center" bgcolor="#ffb953"></td>
-                    <td align="left" width="90%" bgcolor="#ffb953" style="color:#000000;font-size:13px;font-family:Gotham,'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:300;line-height:16px"><i>შეიძინეთ მატარებლის ბილეთები ონლაინ</i></td>
-                    <td width="5%" align="center" bgcolor="#ffb953"></td>
-                </tr>
-                <tr>
-                    <td height="20" width="100%" colspan="3" align="center" bgcolor="#ffb953"></td>
-                </tr>
-                <tr>
-                    <td height="20" width="100%" colspan="3" align="center"></td>
-                </tr>
-                <tr>
-                    <td width="5%" align="center"></td>
-                    <td align="left" width="90%" style="color:#202020;font-size:20px;font-family:Gotham,'Helvetica Neue',Helvetica,Arial,sans-serif;line-height:24px"></td>
-                    <td width="5%" align="center"></td>
-                </tr>
-                <tr>
-                    <td height="20" width="100%" align="left" colspan="3"></td>
-                </tr>
-                <tr>
-                    <td width="5%" align="center"></td>
-                    <td align="left" width="90%" style="color:#202020;font-size:14px;font-family:Gotham,'Helvetica Neue',Helvetica,Arial,sans-serif;line-height:16px">გმადლობთ რომ სარგებლობთ matarebeli.ge-ს მომსახურებით</td>
-                    <td width="5%" align="center"></td>
-                </tr>
-                <tr>
-                    <td height="20" width="100%" align="left" colspan="3"></td>
-                </tr>
-                <tr>
-                    <td height="20" width="100%" align="center" colspan="3"></td>
-                </tr>
-                <tr>
-                    <td width="5%" align="center"></td>
-                    <td align="left" style="color:#202020;font-size:14px;font-family:Gotham,'Helvetica Neue',Helvetica,Arial,sans-serif;line-height:16px">თქვენს მიერ შეძენილი ბილეთ(ებ)ის შესყიდვის კოდი:</td>
-                    <td width="5%" align="center"></td>
-                </tr>
-                <tr>
-                    <td height="20" width="100%" align="left" colspan="3"></td>
-                </tr>
-                <tr>
-                    <td width="5%" align="center"></td>
-                    <td align="left" style="color:#202020;font-size:16px;font-family:Gotham,'Helvetica Neue',Helvetica,Arial,sans-serif;line-height:20px">
-                        @foreach( $transaction->tickets as $key => $ticket )
-                            @if( $key == 0 )
-                                <p><strong>ერთი გზა {{$ticket->request_id}}</strong></p>
-                            @else
-                                <p><strong>მეორე გზა {{$ticket->request_id}}</strong></p>
-                            @endif
-                        @endforeach
-                    </td>
-                    <td width="5%" align="center"></td>
-                </tr>
-                <tr>
-                    <td height="20" width="100%" align="center" colspan="3"></td>
-                </tr>
-                <tr>
-                    <td width="5%" align="center"></td>
-                    <td align="left" width="90%" style="color:#202020;font-size:14px;font-family:Gotham,'Helvetica Neue',Helvetica,Arial,sans-serif;line-height:16px">მიმაგრებულ ფაილში იხილეთ ბილეთის საბეჭდი ვერსია.
-                        <br><br>ელექტრონული სამგზავრო დოკუმენტით მგზავრობისათვის ვაგონის გამცილებელთან საჭიროა
-                        წარადგინოთ მგზავრების პიროვნების დამადასტურებელი მოწმობა.</td>
-                    <td width="5%" align="center"></td>
-                </tr>
-                <tr>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>matarebeli.ge</title>
+    <style>
+        body {background-color: #ebf5f9;}
+        * {margin: 0; padding: 0; box-sizing: border-box;}
+        .mail-wrapper {width: 600px; margin: 30px auto;}
+        .mail-wrapper .logo-wrapper {width: 100%; padding-bottom: 20px; text-align: center;}
+        .mail-wrapper .logo-wrapper img {display: inline-block;}
+        .mail-wrapper .header {height: 60px; width: 100%; background-color: #ffb953; padding: 20px 40px;}
+        .mail-wrapper .header h1 {color: #ffffff; font-family: Arial, Helvetica, sans-serif; font-size: 18px; font-weight: bold; line-height: 20px;}
+        .mail-wrapper .content {width: 100%; height: auto; padding: 50px 100px; background-color: #ffffff;}
+        .mail-wrapper .content h2 {color: #000000; font-family: Arial, Helvetica, sans-serif; font-size: 14px; font-weight: normal; line-height: 20px; margin-bottom: 10px;}
+        .mail-wrapper .content .code {width: 100%; height: 80px; text-align: center; padding: 15px; background-color: #f2f2f2; margin-bottom: 20px;}
+        .mail-wrapper .content .code h3 {color: #000000; font-family: Arial, Helvetica, sans-serif; font-size: 48px; font-weight: normal; line-height: 50px;}
+        .mail-wrapper .content .qr-wrapper {width: 100%; padding: 15px 0 35px; text-align: center;}
+        .mail-wrapper .content .qr-wrapper img {display: inline-block;}
+        .mail-wrapper .content .desc {width: 100%; padding-bottom: 30px}
+        .mail-wrapper .content .desc p {color: #000000; font-family: Arial, Helvetica, sans-serif; font-size: 14px; font-weight: normal; line-height: 18px;margin-bottom: 10px;}
+        .mail-wrapper .content .btn {display: block; width: 100%; height: 80px;color: #000000; font-family: Arial, Helvetica, sans-serif; font-size: 18px; font-weight: bold; text-align: center; text-decoration: none; padding: 30px 0; background-color: #ffb953;}
+        .mail-wrapper .content .btn:hover {background-color: #ffc065;}
+    </style>
+</head>
+<body>
+<div class="mail-wrapper">
+    <div class="logo-wrapper">
+        <img src="images/logo.png" alt="matarebeli.ge">
+    </div>
+    <div class="header">
+        <h1>THANK YOU FOR USING OUR SERVICE</h1>
+    </div>
+    <div class="content">
 
-                    <td height="20" width="100%" align="center" colspan="3"></td>
-                </tr>
-                <tr>
-                    <td height="20" width="100%" align="left" colspan="3"></td>
-                </tr>
-                </tbody></table>
-        </td>
-    </tr>
-    <tr height="50" width="100%">
-        <td align="center">
-            <table>
-                <tbody><tr>
-                    <td height="50" width="100%" align="center"></td>
-                </tr>
-                </tbody></table>
-        </td>
-    </tr>
-    </tbody></table>
+        @foreach( $transaction->tickets as $key => $ticket )
+            @if( $key == 0 )
+                <h2>Departure ticket purchase code</h2>
+                <div class="code">
+                    <h3>{{$ticket->request_id}}</h3>
+                </div>
+            @else
+                <h2>Departure ticket purchase code</h2>
+                <div class="code">
+                    <h3>{{$ticket->request_id}}</h3>
+                </div>
+            @endif
+        @endforeach
+
+        <div class="qr-wrapper">
+            <img src="images/qr.png" alt="matarebeli.ge">
+        </div>
+        <div class="desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </div>
+        <a href="" class="btn">VIEW PRINTABLE TICKETS</a>
+    </div>
+</div>
+</body>
+</html>
