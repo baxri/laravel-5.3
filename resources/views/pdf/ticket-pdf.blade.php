b21f9a4731d06f18e521bd661d0e119bac044072
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-02-10 11:26:35 +0400

    Ticket PDF

--- resources/views/pdf/ticket-pdf.blade.php
+++ resources/views/pdf/ticket-pdf.blade.php
@@ -343,7 +343,7 @@
                     {{ \date('m', strtotime( $ticket->leave_datetime )) }}
                 </strong>
                 <span>
-                    ოქტ
+                    {{\App\helpers\Railway::translateDate($ticket->leave_datetime)}}
                     <br>
                     Oct
                 </span>
