f2fed13ef1a3078b03e87639efac76532e7d8371
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-02-15 14:39:44 +0400

    Payoutable Amount + Fee

--- app/Ticket.php
+++ app/Ticket.php
@@ -421,7 +421,7 @@ class Ticket extends RaModel
 
         $ticket_action_lifetime = config('railway.ticket_action_lifetime');
 
-        if( strlen($this->action_token) != 32 ){
+        if( strlen($this->action_token) < 5 ){
             return false;
         }
 
