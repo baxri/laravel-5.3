6625cf67af76b988cbd11dd07089e1a48477f1df
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-03-01 17:24:04 +0400

    sg sg sg

--- resources/views/vendor/backpack/crud/filters/text.blade.php
+++ resources/views/vendor/backpack/crud/filters/text.blade.php
@@ -26,32 +26,34 @@
             console.log(code);
 
             if(code==13){
-
-                var parameter = $(this).attr('name');
-                var value = $(this).val();
-
-                @if (!$crud->ajaxTable())
-                    var current_url = normalizeAmpersand("{{ Request::fullUrl() }}");
-                    var new_url = addOrUpdateUriParameter(current_url, parameter, value);
-
-                    new_url = normalizeAmpersand(new_url.toString());
-                    window.location.href = new_url.toString();
-                @else
-
-                var ajax_table = $("#crudTable").DataTable();
-                var current_url = ajax_table.ajax.url();
-                var new_url = addOrUpdateUriParameter(current_url, parameter, value);
-
-                new_url = normalizeAmpersand(new_url.toString());
-                ajax_table.ajax.url(new_url).load();
-
-
-                @endif
-
+                makeOP();
             }
 
 
         });
+
+        function makeOP(){
+            var parameter = $(this).attr('name');
+            var value = $(this).val();
+
+                    @if (!$crud->ajaxTable())
+            var current_url = normalizeAmpersand("{{ Request::fullUrl() }}");
+            var new_url = addOrUpdateUriParameter(current_url, parameter, value);
+
+            new_url = normalizeAmpersand(new_url.toString());
+            window.location.href = new_url.toString();
+                    @else
+
+            var ajax_table = $("#crudTable").DataTable();
+            var current_url = ajax_table.ajax.url();
+            var new_url = addOrUpdateUriParameter(current_url, parameter, value);
+
+            new_url = normalizeAmpersand(new_url.toString());
+            ajax_table.ajax.url(new_url).load();
+
+
+            @endif
+        }
 
         $("li[filter-name={{ $filter->name }}]").on('filter:clear', function(e) {
             $(".{{ $filter->name }}-class").val("");
