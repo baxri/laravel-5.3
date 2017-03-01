928c56d6552844d380a5125762481863e393a45c
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-03-01 16:35:41 +0400

    sg sg sg

--- vendor/backpack/crud/src/app/Http/Controllers/CrudFeatures/AjaxTable.php
+++ vendor/backpack/crud/src/app/Http/Controllers/CrudFeatures/AjaxTable.php
@@ -10,6 +10,9 @@ trait AjaxTable
      */
     public function search()
     {
+
+        d('OK');
+
         $this->crud->hasAccessOrFail('list');
 
         // crate an array with the names of the searchable columns
