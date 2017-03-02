5402ecb9e472efa935d7e6503bdad6a4441b0191
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-03-02 17:04:21 +0400

    sg sg sg

--- app/Http/Controllers/Admin/TransactionCrudController.php
+++ app/Http/Controllers/Admin/TransactionCrudController.php
@@ -169,24 +169,7 @@ class TransactionCrudController extends CrudController {
             });*/
 
 
-        $this->crud->addFilter([
-            'type' => 'dropdown',
-            'name' => 'status',
-            'label'=> 'Payment Status'
-        ],
-            [
-                1 => 'Not Payed',
-                -1 => 'Canceled',
-                3 => 'Success',
-                18 => 'Reversed',
-            ],
-            function( $value ) {
-                if (!empty($value))
-                    $this->crud->addClause('where', 'transactions.status', $value);
-                else
-                    $this->crud->addClause('where', 'transactions.status', '<', Transaction::$notfinished);
 
-            });
 
         $this->crud->addFilter([
             'type' => 'text',
@@ -282,6 +265,25 @@ class TransactionCrudController extends CrudController {
 
             });
 
+
+        $this->crud->addFilter([
+            'type' => 'dropdown',
+            'name' => 'status',
+            'label'=> 'Payment Status'
+        ],
+            [
+                1 => 'Not Payed',
+                -1 => 'Canceled',
+                3 => 'Success',
+                18 => 'Reversed',
+            ],
+            function( $value ) {
+                if (!empty($value))
+                    $this->crud->addClause('where', 'transactions.status', $value);
+                else
+                    $this->crud->addClause('where', 'transactions.status', '<', Transaction::$notfinished);
+
+            });
 
         d($this->crud->query->toSql());
 
