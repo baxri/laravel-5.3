5c1e85fa370dc424435af9bf43081d384012ca7b
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-03-06 14:09:08 +0400

    Payout Transaction Description

--- resources/views/vendor/backpack/base/layout.blade.php
+++ resources/views/vendor/backpack/base/layout.blade.php
@@ -41,8 +41,7 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
 
 </head>
 <!-- add this class to close sidebar:  sidebar-collapse  -->
@@ -103,6 +102,8 @@
 
     <!-- jQuery 2.2.0 -->
     <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
     <script>window.jQuery || document.write('<script src="{{ asset('vendor/adminlte') }}/plugins/jQuery/jQuery-2.2.0.min.js"><\/script>')</script>
     <!-- Bootstrap 3.3.5 -->
     <script src="{{ asset('vendor/adminlte') }}/bootstrap/js/bootstrap.min.js"></script>
