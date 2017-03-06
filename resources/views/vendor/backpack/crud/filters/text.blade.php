ae14e6b2b2aa956c4ff5763ad636fc00bb036115
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-03-06 18:33:26 +0400

    Details Row Bug

--- resources/views/vendor/backpack/crud/filters/text.blade.php
+++ resources/views/vendor/backpack/crud/filters/text.blade.php
@@ -27,22 +27,19 @@
             var parameter = elem.attr('name');
             var value = elem.val();
 
-                    @if (!$crud->ajaxTable())
-            var current_url = normalizeAmpersand("{{ Request::fullUrl() }}");
-            var new_url = addOrUpdateUriParameter(current_url, parameter, value);
+            @if (!$crud->ajaxTable())
+                var current_url = normalizeAmpersand("{{ Request::fullUrl() }}");
+                var new_url = addOrUpdateUriParameter(current_url, parameter, value);
 
-            new_url = normalizeAmpersand(new_url.toString());
-            window.location.href = new_url.toString();
-                    @else
+                new_url = normalizeAmpersand(new_url.toString());
+                window.location.href = new_url.toString();
+            @else
+                var ajax_table = $("#crudTable").DataTable();
+                var current_url = ajax_table.ajax.url();
+                var new_url = addOrUpdateUriParameter(current_url, parameter, value);
 
-            var ajax_table = $("#crudTable").DataTable();
-            var current_url = ajax_table.ajax.url();
-            var new_url = addOrUpdateUriParameter(current_url, parameter, value);
-
-            new_url = normalizeAmpersand(new_url.toString());
-            ajax_table.ajax.url(new_url).load();
-
-
+                new_url = normalizeAmpersand(new_url.toString());
+                ajax_table.ajax.url(new_url).load();
             @endif
         }
 
