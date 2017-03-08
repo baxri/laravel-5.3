6a1ae4e34bd80900957b9f9c2ad7c289f9495da5
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-03-08 12:47:48 +0400

    Server Side Export

--- app/Http/Controllers/Admin/TicketCrudController.php
+++ app/Http/Controllers/Admin/TicketCrudController.php
@@ -3,27 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Ticket;
-use Backpack\CRUD\app\Http\Controllers\CrudController;
-
-// VALIDATION: change the requests to match your own file names if you need form validation
 use App\Http\Requests\TicketRequest as StoreRequest;
 use App\Http\Requests\TicketRequest as UpdateRequest;
-use App\Backpack\Crud\AjaxTable;
-use App\Backpack\Crud\MyCrudPanel;
 use Carbon\Carbon;
 
-class TicketCrudController extends CrudController {
-
-    use AjaxTable;
-
-    public $crud;
-
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->crud = new MyCrudPanel();
-    }
+class TicketCrudController extends CustomCrudController {
 
 	public function setUp() {
 
@@ -286,28 +270,6 @@ class TicketCrudController extends CrudController {
         // $this->crud->orderBy();
         // $this->crud->groupBy();
         // $this->crud->limit();
-    }
-
-    /**
-     * Display all rows in the database for this entity.
-     *
-     * @return Response
-     */
-    public function index()
-    {
-        $this->crud->hasAccessOrFail('list');
-
-        $this->data['crud'] = $this->crud;
-        $this->data['title'] = ucfirst($this->crud->entity_name_plural);
-
-        // get all entries if AJAX is not enabled
-        if (! $this->data['crud']->ajaxTable()) {
-            $this->data['entries'] = $this->data['crud']->getEntries();
-        }
-
-        // load the view from /resources/views/vendor/backpack/crud/ if it exists, otherwise load the one in the package
-        // $this->crud->getListView() returns 'list' by default, or 'list_ajax' if ajax was enabled
-        return view('vendor.backpack.crud.list', $this->data);
     }
 
 	public function store(StoreRequest $request)
