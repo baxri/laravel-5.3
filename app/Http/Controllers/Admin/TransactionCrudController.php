2f5b2296232481865baab26cdf87ca34e9d55001
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-03-09 16:24:37 +0400

    Change DataTable Loader

--- app/Http/Controllers/Admin/TransactionCrudController.php
+++ app/Http/Controllers/Admin/TransactionCrudController.php
@@ -166,7 +166,7 @@ class TransactionCrudController extends CustomCrudController {
         $this->crud->query->leftjoin('tickets', 'transactions.id', '=', 'tickets.transaction_id');
         $this->crud->query->leftjoin('persons', 'tickets.id', '=', 'persons.ticket_id');
 
-        //$this->crud->query->groupBy('transactions.id');
+        $this->crud->query->groupBy('transactions.id');
 
         $this->crud->addFilter([
             'type' => 'date',
