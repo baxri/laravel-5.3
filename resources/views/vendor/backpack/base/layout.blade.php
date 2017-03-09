746b73030f20ac98f70f9132b3d52a7c2df8c8d7
Giorgi Bibilashvili <gbibilashvili@unipay.ge>
2017-03-09 14:15:27 +0400

    Change DataTable Loader

--- resources/views/vendor/backpack/base/layout.blade.php
+++ resources/views/vendor/backpack/base/layout.blade.php
@@ -48,7 +48,7 @@
             background: white;
             background-image: url(<?php url('public') ?>"/images/7.gif");
             background-repeat: no-repeat;
-            background-position: 50% 10%, 50%, 90%;
+            background-position: 50% 20%, 50%, 80%;
             border: none;
             height: 79%;
             line-height: 180px;
